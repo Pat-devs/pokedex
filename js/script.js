a539c2182ef879a0fb3f04ee323c887dd441ce1d
Pat-devs <patryk.kodehode@gmail.com>
2025-04-03 09:50:14 +0200

    setup static html content

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ const apiUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
 
 
 
-
-async function getData() {
-    const request = await fetch(apiUrl)
+// gets data from a given url param
+// returns a promise with data or an error
+async function getData(url) {
+    const request = await fetch(url)
     const data = await request.json()
 
     //console.log(data)
+    return data
 }
-getData()
+
+let myData = getData(apiUrl)
+console.log(myData)
