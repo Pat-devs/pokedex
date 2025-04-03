d30c9ada4cbb2fd0d7ab9d367f25a7d12f998e20
Pat-devs <patryk.kodehode@gmail.com>
2025-04-03 10:03:55 +0200

    begin creating DOM-related functions

--- js/script.js
+++ js/script.js
@@ -13,5 +13,40 @@ async function getData(url) {
     return data
 }
 
-let myData = getData(apiUrl)
-console.log(myData)
+//let myData = getData(apiUrl)
+//console.log(myData)
+
+// get html elements
+const mainContainerEl = document.getElementById("main-container")
+
+
+
+// displays details about a pokemon in the mainContainerEl
+function displayPokemon() {
+
+}
+
+// displays a list of pokemons in the mainContainerEl
+function displayPokemonList(pokemonList) {
+
+    let onePokemon = pokemonList[0]
+
+    //mainContainerEl.innerHTML += onePokemon.name
+
+    document.createElement("h3") // for name
+    // later on: img tag, list of some stats, url link to pokemom-detail-page???
+}
+
+let testPokemonList = [
+    {
+      name: "bulbasaur",
+      url: "https://pokeapi.co/api/v2/pokemon/1/"
+    },
+    {
+      name: "ivysaur",
+      url: "https://pokeapi.co/api/v2/pokemon/2/"
+    }
+]
+
+
+displayPokemonList(testPokemonList)
